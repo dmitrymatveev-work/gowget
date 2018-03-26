9a72feacdadfc2ac5191fdfc9aece2ac7ac9b8f6
Dmitry Matveyev <dmitry.matveyev.job@gmail.com>
2018-03-26 20:23:49 +0300

    Print status

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"fmt"
 	"os"
 	"sync"
+	"time"
 )
+
+var statuses map[string]int = make(map[string]int)
 
 func main() {
 	args := os.Args[1:]
@@ -16,12 +19,32 @@ func main() {
 	var wg sync.WaitGroup
 	wg.Add(len(args))
 
+	for _, a := range args {
+		statuses[a] = 0
+	}
+
 	go func() {
-		for _, a := range os.Args[1:] {
-			fmt.Println(a)
+		for {
+			printStatus()
+			time.Sleep(time.Second)
+		}
+	}()
+
+	go func() {
+		for _, a := range args {
+			statuses[a] = 100
+			time.Sleep(2*time.Second)
 			wg.Done()
 		}
 	}()
 
 	wg.Wait()
+}
+
+func printStatus() {
+	var result string
+	for _, v := range statuses {
+		result = fmt.Sprintf("%s\t%d%%", result, v)
+	}
+	fmt.Println(result)
 }
